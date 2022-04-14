47326e065a5c2bb3ea25b13637ad0fb2752bb511
icy121yci <101975368+icy121yci@users.noreply.github.com>
2022-04-14 17:36:54 +0800

    栈
    栈

--- header.h
+++ header.h
@@ -1,144 +1,38 @@
-/***************************************************************************************
- *    File Name                :    LQueue.h
- *    CopyRight                :
- *
- *    SYSTEM                    :   Mac OS
- *    Create Data                :    2020.4.2
- *    Author/Corportation        :   Chuan Shi
- *
- *
- *--------------------------------Revision History--------------------------------------
- *    No    version        Data            Revised By            Item            Description
- *
- *
- ***************************************************************************************/
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
 
-/**************************************************************
- *    Multi-Include-Prevent Section
- **************************************************************/
-#ifndef LQUEUE_H
-#define LQUEUE_H
-#define CHAR_MAX 10
-/**************************************************************
- *    Struct Define Section
- **************************************************************/
-//链式队列结构
-typedef struct node
+typedef enum Status 
 {
-    void *data;                   //数据域指针
-    struct node *next;            //指向当前结点的下一结点
-    int datatype;// 存放数据类型 
-} Node;
-
-typedef struct Lqueue
-{
-    Node *front;                   //队头
-    Node *rear;                    //队尾
-    int length;            //队列长度
-} LQueue;
-
-typedef enum
-{
-    FALSE=0, TRUE=1
+    ERROR = 0, 
+	SUCCESS = 1
 } Status;
-					
-			
 
-/**************************************************************
- *    Prototype Declare Section
- **************************************************************/
+typedef int ElemType;
 
+typedef  struct StackNode
+{
+	ElemType data;
+	struct StackNode *next;
+}StackNode, *LinkStackPtr;
 
-/**
- *  @name        : void InitLQueue(LQueue *Q)
- *    @description : 初始化队列
- *    @param         Q 队列指针Q
- *  @notice      : None
- */
-void InitLQueue(LQueue *Q);//
-
-/**
- *  @name        : void DestoryLQueue(LQueue *Q)
- *    @description : 销毁队列
- *    @param         Q 队列指针Q
- *  @notice      : None
- */
-void DestoryLQueue(LQueue *Q);
-
-/**
- *  @name        : Status IsEmptyLQueue(const LQueue *Q)
- *    @description : 检查队列是否为空
- *    @param         Q 队列指针Q
- *    @return         : 空-TRUE; 未空-FALSE
- *  @notice      : None
- */
-Status IsEmptyLQueue(LQueue *Q);//
-
-/**
- *  @name        : Status GetHeadLQueue(LQueue *Q, void *e)
- *    @description : 查看队头元素
- *    @param         Q e 队列指针Q,返回数据指针e
- *    @return         : 成功-TRUE; 失败-FALSE
- *  @notice      : 队列是否空
- */
-Status GetHeadLQueue(LQueue *Q);//
-
-/**
- *  @name        : int LengthLQueue(LQueue *Q)
- *    @description : 确定队列长度
- *    @param         Q 队列指针Q
- *    @return         : 成功-TRUE; 失败-FALSE
- *  @notice      : None
- */
-int LengthLQueue(LQueue *Q);//
-
-/**
- *  @name        : Status EnLQueue(LQueue *Q, void *data)
- *    @description : 入队操作
- *    @param         Q 队列指针Q,入队数据指针data
- *    @return         : 成功-TRUE; 失败-FALSE
- *  @notice      : 队列是否为空
- */
-Status EnLQueue(LQueue *Q);//
-
-/**
- *  @name        : Status DeLQueue(LQueue *Q)
- *    @description : 出队操作
- *    @param         Q 队列指针Q
- *    @return         : 成功-TRUE; 失败-FALSE
- *  @notice      : None
- */
-Status DeLQueue(LQueue *Q);//
-
-/**
- *  @name        : void ClearLQueue(AQueue *Q)
- *    @description : 清空队列
- *    @param         Q 队列指针Q
- *  @notice      : None
- */
-void ClearLQueue(LQueue *Q);//
-
-/**
- *  @name        : Status TraverseLQueue(const LQueue *Q, void (*foo)(void *q))
- *    @description : 遍历函数操作
- *    @param         Q 队列指针Q，操作函数指针foo
- *    @return         : None
- *  @notice      : None 
- */
-Status TraverseLQueue(LQueue *Q, void (*foo)(void *q,int type));//
-
-/**
- *  @name        : void LPrint(void *q)
- *    @description : 操作函数
- *    @param         q 指针q
- 
- *  @notice      : None
- */
-void LPrint(void *q,int type);//
-
-/**************************************************************
- *    End-Multi-Include-Prevent Section
- **************************************************************/
-#endif // LQUEUE_H_INCLUDED
+typedef  struct  LinkStack
+{
+	LinkStackPtr top;
+	int	count;
+}LinkStack;
 
 
+
+//链栈
+Status initLStack(LinkStack *s);//初始化栈
+Status isEmptyLStack(LinkStack *s);//判断栈是否为空
+Status getTopLStack(LinkStack *s);//得到栈顶元素
+Status clearLStack(LinkStack *s);//清空栈
+Status destroyLStack(LinkStack *s);//销毁栈
+Status tackTraverse(LinkStack *s);//打印栈链 
+Status LStackLength(LinkStack *s);//检测栈长度
+Status pushLStack(LinkStack *s,ElemType data);//入栈
+Status popLStack(LinkStack *s);//出栈
+
+
+#endif 
