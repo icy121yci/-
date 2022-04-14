626989a65d11691e8ba3d017c669024643a84b5b
icy121yci <101975368+icy121yci@users.noreply.github.com>
2022-04-14 17:23:48 +0800

    Add files via upload

--- source (2).c
+++ source (2).c
@@ -1,81 +1,81 @@
-#include "LQueue.h" 
-#include "stdio.h"    
-#include "stdlib.h"
+#include "../head/LinkStack.h" 
+#include<stdio.h>
+#include <iostream> 
 int main (){
-	int menu;
- 	int flag=1;
-	LQueue Q;	
-	while(flag) {
-
-		printf("-------------菜单-------------\n");
-		printf("--------1.初始化队列----------\n");
-		printf("--------2.入队列--------------\n");
-		printf("--------3.出队列--------------\n");
-		printf("--------4.打印队列------------\n");
-		printf("--------5.判断是否为空队列----\n");	
-		printf("--------6.查看队头元素--------\n");
-		printf("--------7.确定队列长度--------\n");
-		printf("--------8.清空队列------------\n");
-		printf("--------9.销毁队列并退出系统--\n");
+	int menu,flag2=1;
+	ElemType flag; 
+	LinkStackPtr L;
+	LinkStack s; 
+	while(flag2){
+	
+		printf("-----------菜单---------\n");
+		printf("-------1.初始化栈-------\n") ;
+		printf("-------2.入栈-----------\n") ;
+		printf("-------3.出栈-----------\n") ;
+		printf("-------4.打印栈---------\n") ;
+		printf("-------5.判断栈是否为空-\n") ;
+		printf("-------6.得到栈顶元素---\n") ;
+		printf("-------7.检测栈长度-----\n") ;
+		printf("-------8.清空栈---------\n") ;
+		printf("-------9.退出系统-------\n") ;
 		
 		printf("请输入数字(1~9)：\n");
 		scanf("%d",&menu);
 		switch(menu){
-				case 1: 
-					InitLQueue(&Q);
-					system("pause");
-					system("cls");				 
-					break;
-				case 2:			 
-					EnLQueue(&Q) ;
-					system("pause");
-					system("cls");
-					break;
-						
-				case 3:
-					DeLQueue(&Q);
-					system("pause");
-					system("cls"); 
-					break;
-				case 4:
+			case 1: 
+				initLStack(&s); 
+				system("pause");
+				system("cls");				 
+				break; 
+			case 2:
+				printf("请输入数据：\n");
+				scanf("%d",&flag);
+				pushLStack(&s,flag); 
+				system("pause");
+				system("cls");	
+				break;
+			case 3:
+				popLStack(&s)	;
+				system("pause");
+				system("cls");	
+				break;
+			case 4:
+				tackTraverse(&s) ;
+				system("pause");
+				system("cls");	
+				break;
+			case 5:
+				isEmptyLStack(&s);
+				system("pause");
+				system("cls");	
+				break;	
+			case 6:
+				getTopLStack(&s);
+				system("pause");
+				system("cls");	
+				break;
+			case 7:
+				LStackLength(&s); 
+				system("pause");
+				system("cls");	
+				break;
+			case 8:
+				clearLStack(&s);
+				system("pause");
+				system("cls");	
+				break;
+			case 9:
+				destroyLStack(&s);
+				flag2=0;
+				system("pause");
+				system("cls");	
+				break;
+			default:
+				printf("请输入数字(1~9)\n");
+				system("pause");
+				system("cls");	
 					
-					TraverseLQueue( &Q, LPrint);
-					system("pause");
-					system("cls");
-					break;
-				case 5:
-					IsEmptyLQueue( &Q); 
-					system("pause");
-					system("cls");
-					break;
-				case 6:
-					GetHeadLQueue(&Q);
-					system("pause");
-					system("cls"); 
-					break;
-				case 7:
-					menu=LengthLQueue(&Q); 
-					printf("长度为：%d\n",menu);
-					system("pause");
-					system("cls");
-					break;
-				case 8:
-					ClearLQueue(&Q) ;
-					system("pause");
-					system("cls");
-					break;
-				case 9:
-					DestoryLQueue(&Q);
-					flag=0;
-					system("pause");
-					system("cls");
-					break;
-				default:
-					printf("请输入1~9的数字\n");
-					system("pause");
-					
-					system("cls"); 
-		}
+			}
 	}
 	return 0;
-}
+} 
